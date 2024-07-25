552198c89328cbdf8e38df07481fb07338d4fb94
Cold Tank <coldalt225@gmail.com>
2024-07-25 17:22:01 +0400

    first commit

---
+++ fetch_playlist.py
@@ -0,0 +1,31 @@
+''' This is the script that will fetch the spotify playlist and add the tracks to a text file. '''
+
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URL = os.getenv('REDIRECT_URL')
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URL,
+                                               scope="playlist-read-private"))
+
+def fetch_sp_pl(link: str):
+    playlist_url = link
+    playlist = sp.playlist(playlist_url)
+
+    for name in playlist['tracks']['items']:
+        print("Song name: ", name['track']['name'])
+        print("Artist: ", name['track']['artists'][0]['name'])
+
+        try:
+            os.mkdir(os.getcwd() + '/results/')
+        except FileExistsError:
+            with open(os.getcwd() + '/results/sp_result.txt', 'a') as f:
+                f.write(f"{name['track']['name'] + ' ' + name['track']['artists'][0]['name']}\n")
---
+++ spotify2yt.py
@@ -0,0 +1,6 @@
+import sys
+from yt_playlist import get_link
+from fetch_playlist import fetch_sp_pl
+
+sp_link = sys.argv[1]
+print(sp_link)
